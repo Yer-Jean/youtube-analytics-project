e5945f68776d806fa2e9dc9cbd64f7da5dbb445c
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-07-18 12:03:17 +0600

    Class Video modifyed

--- src/video.py
+++ src/video.py
@@ -7,13 +7,20 @@ class Video(APIMixin):
     def __init__(self, video_id: str):
         self.__video_id: str = video_id
         self.video_info = self.get_service().videos().list(part='snippet,statistics', id=self.__video_id).execute()
-        self.video_title: str = self.video_info['items'][0]['snippet']['title']
-        self.video_url: str = 'https://www.youtube.com/watch?v=' + self.__video_id
-        self.view_count: int = int(self.video_info['items'][0]['statistics']['viewCount'])
-        self.like_count: int = int(self.video_info['items'][0]['statistics']['likeCount'])
+        try:
+            self.title: str = self.video_info['items'][0]['snippet']['title']
+        except IndexError:
+            self.title: str = None
+            self.url: str = None
+            self.view_count: int = None
+            self.like_count: int = None
+        else:
+            self.url: str = 'https://www.youtube.com/watch?v=' + self.__video_id
+            self.view_count: int = int(self.video_info['items'][0]['statistics']['viewCount'])
+            self.like_count: int = int(self.video_info['items'][0]['statistics']['likeCount'])
 
     def __str__(self):
-        return f'{self.video_title}'
+        return f'{self.title}'
 
     @property
     def video_id(self):
